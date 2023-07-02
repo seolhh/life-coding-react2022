f6958813a6c27bf89520c561887c40f445b0a9bc
seolhyehyeon <seolhyehyeon@gmail.com>
2023-07-02 23:44:57 +0900

    add react props

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a href={"/read/" + t.id}>{t.title}</a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      Welcome, {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "javascript", body: "javascript is..." },
+  ];
+
+  return (
+    <div>
+      <Header title="강아지"></Header>
+      <Article title="슈나우저" body="스벤"></Article>
+      <Article title="말티즈" body="비누"></Article>
+      <Nav topics={topics}></Nav>
+    </div>
+  );
+}
+
+export default App;
